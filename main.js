7a95cb4efaed55f52e0014c5b1c47e0f546439d4
simon-off <simon-off@users.noreply.github.com>
2024-04-06 11:45:47 +0200

    Update fonts

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import "@fontsource/poppins/900-italic.css";
-import "@fontsource/rubik";
-import "@fontsource/rubik/300.css";
-import "@fontsource/jetbrains-mono";
-import "@fontsource/jetbrains-mono/500.css";
+import "@fontsource-variable/rubik";
+import "@fontsource-variable/jetbrains-mono";
 import "./scss/main.scss";
 import "./js/zigzag";
 import "./js/onload";
