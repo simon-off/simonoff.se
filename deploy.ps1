aaf05dca254ce26989e055b6d110c436f07d3661
simon-off <simon-off@users.noreply.github.com>
2025-01-17 20:40:53 +0100

    Update deploy script

--- deploy.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Building site..."
-pnpm run build
-Copy-Item -r dist public_html
-
-Write-Host "Copying files to server..."
-scp -r public_html simonoff:~/
-Remove-Item -r public_html
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+HOST="simonoff"
+
+echo "Building site..."
+pnpm run build
+
+# Exit if build fails
+if [ $? -ne 0 ]; then
+    echo "Build failed. Exiting deployment process."
+    exit 1
+fi
+
+# Execute dry-run to show what will be transferred
+echo "Executing dry-run..."
+rsync -avh --delete --dry-run --exclude 'wehatemusic/' --exclude '.*' dist/ $HOST:~/public_html
+
+# Ask for confirmation before proceeding with the deployment
+read -p "Do you want to proceed with the deployment? (yes/no) [yes]: " confirmation
+
+confirmation=${confirmation:-yes}
+
+if [[ "$confirmation" =~ ^[Yy](es)?$ ]]; then
+    echo "Deploying to server using rsync..."
+    rsync -ah --delete --exclude 'wehatemusic/' --exclude '.*' dist/ $HOST:~/public_html
+    echo "Deployment complete."
+else
+    echo "Deployment cancelled."
+fi
